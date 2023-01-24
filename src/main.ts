0c32491bece3099cdcf415290ba8a7729a469125
Alex <alex.alekseichuk@gmail.com>
2023-01-24 16:28:42 +0300

    logger based on winston

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { createLogger } from './logger';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
+    logger: createLogger(),
   });
   await app.listen();
 }
