ef6a2119d919586e17ea0451bb5314a35d745044
Alexey Ivanov <alexey.ivanov>
2023-03-17 00:21:03 +0300

    Provide default logger features into template service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { logger } from '@1win/cdp-backend-tools';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './configuration/config';
+import { GlobalExceptionFilter } from './filter';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // Remove it if your app doesn't handle http server
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger.LoggerMiddleware(config())).forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
